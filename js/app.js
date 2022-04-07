3bc2f3f7dfd1e34842995a20ec88141bf5b26b54
mauroaveron <mauroaveron@gmail.com>
2022-04-07 02:08:11 -0300

    new commit

--- js/app.js
+++ js/app.js
@@ -15,6 +15,12 @@ function cargarEventListeners() {
     // Elimina cursos del carrito 
     carrito.addEventListener('click', eliminarCurso);
 
+    // Muestra los cursos de Local Storage
+    document.addEventListener('DOMContentLoaded', () => {
+        articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        carritoHTML();
+    });
+
     // Vaciar carrito
     vaciarCarritoBtn.addEventListener('click', () => {
         articulosCarrito = []; // reseteamos el arreglo
@@ -104,6 +110,13 @@ function carritoHTML() {
         // Agrega el HTML del carrito en el tbody
         contenedorCarrito.appendChild(row);
     })
+
+    // Agregar el carrito de compras al Local Storage
+    sincronizarStorage();
+}
+
+function sincronizarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 // Elimina los cursos del tbody
